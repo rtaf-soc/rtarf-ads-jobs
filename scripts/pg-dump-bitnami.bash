ab13570e421f9e8879d1d5b3c582fa9301e197e7
pjames <pjames@gmail.com>
2025-05-23 13:15:58 +0700

    Added exclude data for pg_dump

--- scripts/pg-dump-bitnami.bash
+++ scripts/pg-dump-bitnami.bash
@@ -3,10 +3,18 @@
 POSTGRES_USER=$1
 OUT_FILE_NAME=$2
 TARGET_DIR=$3
+EXCLUDE_FLAG=$4
 
 DB=${POSTGRES_DB}
 if [ -z "${DB}" ]; then
     DB=${POSTGRES_DATABASE}
+fi
+
+FLAG1=${EXCLUDE_FLAG}
+if [ -z "${FLAG1}" ]; then
+    FLAG1=""
+else
+    FLAG1="--exclude-table-data='public.\"LogAggregates\"'"
 fi
 
 echo "In [pg-dump-bitnami.bash]"
@@ -19,5 +27,5 @@ echo "DB=[${DB}]"
 cd ${TARGET_DIR}
 # Delete the previous backup file
 rm -f *.sql *.gz
-pg_dump -c --no-owner --dbname="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${DB}" > ${OUT_FILE_NAME}
+pg_dump -c --no-owner ${FLAG1} --dbname="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${DB}" > ${OUT_FILE_NAME}
 gzip ${OUT_FILE_NAME}
