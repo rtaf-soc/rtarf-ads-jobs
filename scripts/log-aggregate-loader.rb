c57deade2bef9a26b96a90b39937148bb7816c09
pjames <pjames@gmail.com>
2025-11-24 16:14:32 +0700

    Added aggre type aggr_crowdstrike_incident_mitre_v1

--- scripts/log-aggregate-loader.rb
+++ scripts/log-aggregate-loader.rb
@@ -176,6 +176,11 @@ def upsertData(dbConn, type, keyword, aggrCount, seq)
     end
 
     populateMachineStat(csComputerName, zone, aggrCount)
+  elsif (type == 'aggr_crowdstrike_incident_mitre_v1')
+    dataSet, srcNetwork, dstNetwork, protocol, transport,
+    customField1, customField2, customField3, customField4, customField5, customField6,
+    customField7, customField8, customField9, customField10, customField11, customField12 = attributes.split("^")
+    #Custom Fields : aggrId,eventName,serverity,serverityName,riskScore,tactic,tacticMitre,tacticIdMitre,technique,techniqueMitre,techniqueIdMitre,techniqueIdMitreNormalize
   elsif (type == 'aggr_zeek_intel_v1')
     dataSet, srcNetwork, dstNetwork, protocol, transport, srcIp, dstIp,
     customField1, customField2, customField3, customField4, customField5 = attributes.split("^")
@@ -436,6 +441,10 @@ totalLoad = load_log_aggregate(conn, redis, type)
 puts("INFO : ### Done loading [#{type}] [#{totalLoad}] records to PostgreSQL\n")
 # Onlye for aggr_crowdstrike_incident_v1
 loadMachineStatToDb(conn)
+
+type = 'aggr_crowdstrike_incident_mitre_v1'
+totalLoad = load_log_aggregate(conn, redis, type)
+puts("INFO : ### Done loading [#{type}] [#{totalLoad}] records to PostgreSQL\n")
 
 type = 'aggr_panw_v1'
 totalLoad = load_log_aggregate(conn, redis, type)
